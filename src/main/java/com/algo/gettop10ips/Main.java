49a17048becb761b258cb199f472160f906c1f44
liuhanyu <liuhanyu@chinalife.com.hk>
2019-12-13 17:59:39 +0800

    +testTokenizer

--- src/main/java/com/algo/gettop10ips/Main.java
+++ src/main/java/com/algo/gettop10ips/Main.java
@@ -2,20 +2,38 @@ package com.algo.gettop10ips;
 
 import java.io.IOException;
 import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import com.algo.sharding.IPShardingCounter;
 
 public class Main {
     public static void main( String[] args ) throws IOException
     {
-    	IPTokenizer ipc = new IPTokenizer("access.log");
-    	IPShardingCounter ipSharding = new IPShardingCounter();
-    	ipc.OpenAccessLog();
-    	String curIp;
-    	while ((curIp = ipc.getNextIp()) != null)
+    	IPTokenizer []ipc = new IPTokenizer[4];
+    	ExecutorService e = Executors.newFixedThreadPool(ipc.length);
+    	for (int i = 0; i < ipc.length; i++)
     	{
-    		ipSharding.inc(curIp);
+    		ipc[i] = new IPTokenizer("access.log", ipc.length, i);
     	}
+    	
+    	for (int i = 0; i < ipc.length; i++)
+    	{
+    		final IPTokenizer x = ipc[i];
+    		e.submit(() -> { try {
+    			String ss;
+				while ( (ss = x.getNextIp()) != null) {
+					//System.out.println(ss);
+				}
+			} catch (Exception e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}; });
+    	}
+    	
+    	IPShardingCounter ipSharding = new IPShardingCounter();
+    	String curIp;
+
     	Map<String, Integer> res = ipSharding.getTopnAccessdIPs(40, 10);
     	System.out.println(res);
     }
